b465ec2501bd7de4527fbdccbb8c81e921dc657d
raikoug <raikoug@gmail.com>
2024-12-02 16:18:44 +0100

    that monoline was awfull. p2 day 2

--- 2024/solutions/python/day_02.py
+++ 2024/solutions/python/day_02.py
@@ -4,30 +4,47 @@ from pathlib import Path
 CURRENT_DAY = int(Path(__file__).stem.replace('day_',''))
 aoc = AOC(CURRENT_YEAR)
 
+def check_numbers(ns: list) -> bool:
+    coppie = [[ns[i],ns[i+1]] for i in range(0,len(ns)-1)]
+    good = True
+    # se fosse crescente:
+    if coppie[0][0] < coppie[0][1]: # crescente
+        diff = [el[1]-el[0] for el in coppie]
+    else:
+        diff = [el[0]-el[1] for el in coppie]
+        
+    for d in diff:
+        if (d > 3) or (d < 1):
+            good = False
+            break
+    
+    return good
+
 def solve_1(test_string = None) -> int:
     inputs_1 = aoc.get_input(CURRENT_DAY, 1) if not test_string else test_string
     safe = 0
     for line in inputs_1.splitlines():
         ns = [int(el) for el in line.split(" ")]
-        coppie = [[ns[i],ns[i+1]] for i in range(0,len(ns)-1)]
-        good = True
-        # se fosse crescente:
-        if coppie[0][0] < coppie[0][1]: # crescente
-            diff = [el[1]-el[0] for el in coppie]
-        else:
-            diff = [el[0]-el[1] for el in coppie]
-            
-        for d in diff:
-            if (d > 3) or (d < 1):
-                good = False
-        if good: safe +=1 
+        if check_numbers(ns):
+            safe += 1 
         
     return safe
     
 def solve_2(test_string = None) -> int:
     inputs_1 = aoc.get_input(CURRENT_DAY, 1) if not test_string else test_string
     safe = 0
-    print(*[sum(x) for x in zip(*[((lambda l: (lambda ds: (all(d > 0 for d in ds) or all(d < 0 for d in ds))and all(1 <= abs(d) <= 3 for d in ds) and all(d != 0 for d in ds))([l[i+1]-l[i] for i in range(len(l)-1)]))(list(map(int, line.strip().split()))),(lambda l: (lambda s: s(l) or any(s(l[:i]+l[i+1:]) for i in range(len(l))))(lambda l: (lambda ds: (all(d > 0 for d in ds) or all(d < 0 for d in ds))and all(1 <= abs(d) <= 3 for d in ds) and all(d != 0 for d in ds))([l[i+1]-l[i] for i in range(len(l)-1)])))(list(map(int, line.strip().split())))) for line in inputs_1.splitlines()])])
+    for line in inputs_1.splitlines():
+        ns = [int(el) for el in line.split(" ")]
+        if check_numbers(ns):
+            safe += 1
+            continue
+        for i in range(len(ns)):
+            tmp_row = ns[:i] + ns[i+1:]
+            if check_numbers(tmp_row):
+                safe += 1
+                break
+    
+    return safe
 
 if __name__ == "__main__":
     test = """7 6 4 2 1
