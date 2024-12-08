fb6370a1eeb0a5037f8c03baf71fa445268ab01e
raikoug <raikoug@gmail.com>
2024-12-08 14:48:34 +0100

    Add initial implementation for Day 7 solutions in Rust

---
+++ 2024/solutions/rust_sol/src/days/day_07.rs
@@ -0,0 +1,14 @@
+
+pub fn _part_1(_input: &str) {
+    println!("Day 7, Parte 1: ");
+}
+
+pub fn _part_2(_input: &str) {
+    println!("Day 7, Parte 2: ");
+
+}
+
+pub fn start(input: &str){
+    _part_1(input);
+    _part_2(input);
+}
