724093a09917cccb26b6c88cbf4ede9f94797bd1
raikoug <raikoug@gmail.com>
2024-12-11 09:55:53 +0100

    Add initial implementation for day 10 in Rust

---
+++ 2024/solutions/rust_sol/src/days/day_10.rs
@@ -0,0 +1,14 @@
+
+pub fn _part_1(_input: &str) {
+    println!("Day N, Parte 1: ");
+}
+
+pub fn _part_2(_input: &str) {
+    println!("Day N, Parte 2: ");
+
+}
+
+pub fn start(input: &str){
+    _part_1(input);
+    _part_2(input);
+}
